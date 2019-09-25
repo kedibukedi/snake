a7427035a30300dbae654387cd971e42c84cb990
zhangfuyu <960755908@qq.com>
2019-09-25 21:31:16 +0800

    Initial commit

---
+++ src/main/java/com/zfy/App.java
@@ -0,0 +1,13 @@
+package com.zfy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/zfy/Testclass.java
@@ -0,0 +1,10 @@
+package com.zfy;
+
+import org.junit.Test;
+
+public class Testclass {
+    @Test
+    public  void test1(){
+        System.out.println("你好 世界");
+    }
+}
